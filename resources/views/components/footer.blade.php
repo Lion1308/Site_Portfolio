cf8586182457839869b4c64743f326431227f1b1
lion <pavlusha130892@gmail.com>
2023-03-18 14:48:11 +0000

    added footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -11,7 +11,7 @@
             <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
                 <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
             </a>
-            <span class="text-muted">© 2023 All Rights Reserved</span>
+            <span class="text-muted">© 2023 All Rights Reserved, мазафака!</span>
         </div>
 
         <ul class="nav col-md-4 justify-content-end align-items-center list-unstyled d-flex">

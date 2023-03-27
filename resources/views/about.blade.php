94b1df735718ce6f82c44e74a04338e4cb1da483
lion <pavlusha130892@gmail.com>
2023-03-27 17:53:25 +0100

    change styles

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -26,61 +26,64 @@
     {{--    Start Skills--}}
     <div class="skills py-5">
         <div class="skills-container container row mx-auto m-5">
-            <div class="skills-title container my-5">
+                <div class="skills-title container my-5">
                 <h1>Graduate Lisbon School of Design</h1>
                 <h4>Course Motion design</h4>
             </div>
-            <div class="col-md-4 col-sm-12">
-                <p class="">My Skills:</p>
-                <ul class="list-unstyled">
-                    <li>Explainer video</li>
-                    <li>2d Character animation</li>
-                    <li>Promo video</li>
-                    <li>Animation for social media</li>
-                    <li>GIF for web site</li>
-                    <li>Infographics</li>
-                    <li>Kinetic typography</li>
-                    <li>Logo animation</li>
-                    <li>Intro (outro)</li>
-                    <li>Interactive books for
-                        children
-                    </li>
-                    <li>Video editing</li>
-                    <li>Titles</li>
-                    <li>Sound design</li>
-                    <li>Storyboard</li>
-                    <li>Styleframes</li>
-                    <li>Character design</li>
-                    <li>Illustrations for web site</li>
-                    <li>Vector illustrations</li>
-                </ul>
+            <div class="d-flex flex-column flex-lg-row justify-content-evenly">
+                <div class="">
+                    <p class="">My Skills:</p>
+                    <ul class="list-unstyled">
+                        <li>Explainer video</li>
+                        <li>2d Character animation</li>
+                        <li>Promo video</li>
+                        <li>Animation for social media</li>
+                        <li>GIF for web site</li>
+                        <li>Infographics</li>
+                        <li>Kinetic typography</li>
+                        <li>Logo animation</li>
+                        <li>Intro (outro)</li>
+                        <li>Interactive books for
+                            children
+                        </li>
+                        <li>Video editing</li>
+                        <li>Titles</li>
+                        <li>Sound design</li>
+                        <li>Storyboard</li>
+                        <li>Styleframes</li>
+                        <li>Character design</li>
+                        <li>Illustrations for web site</li>
+                        <li>Vector illustrations</li>
+                    </ul>
+                </div>
+                <div style="flex-basis: 25%;">
+                    <p class="light"> </p>
+                    <ul class="list-unstyled">
+                        <li>Editing of all types of footage</li>
+                        <li>Adding signatures, graphics, developing intros and endings, etc.</li>
+                        <li>Subtitles</li>
+                        <li>Video to audio transitions</li>
+                        <li>Promotional video of services</li>
+                        <li>Promo video of a popular product</li>
+                        <li>Site presentation</li>
+                        <li>Promo video for the event</li>
+                        <li>Synchronization of audio and video</li>
+                        <li>Create a slideshow from photos</li>
+                        <li>Color correction</li>
+                        <li>Vacation video</li>
+                    </ul>
+                </div>
+                <div class="">
+                    <p class="skills_MP">Tools I use:</p>
+                    <ul class="list-unstyled">
+                        <li>Adobe After Effects</li>
+                        <li>Adobe Illustrator</li>
+                        <li>Adobe Photoshop</li>
+                        <li>Adobe Premiere Pro</li>
+                    </ul>
+                </div>
             </div>
-            <div class="col-md-4 col-sm-12">
-                <p class="light"> </p>
-                <ul class="list-unstyled">
-                    <li>Editing of all types of footage</li>
-                    <li>Adding signatures, graphics, developing intros and endings, etc.</li>
-                    <li>Subtitles</li>
-                    <li>Video to audio transitions</li>
-                    <li>Promotional video of services</li>
-                    <li>Promo video of a popular product</li>
-                    <li>Site presentation</li>
-                    <li>Promo video for the event</li>
-                    <li>Synchronization of audio and video</li>
-                    <li>Create a slideshow from photos</li>
-                    <li>Color correction</li>
-                    <li>Vacation video</li>
-                </ul>
-            </div>
-            <div class="col-md-4 col-sm-12">
-                <p class="skills_MP">Tools I use:</p>
-                <ul class="list-unstyled">
-                    <li>Adobe After Effects</li>
-                    <li>Adobe Illustrator</li>
-                    <li>Adobe Photoshop</li>
-                    <li>Adobe Premiere Pro</li>
-                </ul>
-            </div>
+
         </div>
         <div class="skills-bg container">
             <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/elements/element8.png') }}" alt="">
